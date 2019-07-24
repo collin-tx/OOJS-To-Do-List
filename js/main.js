ab32809cbb83a53a60f6dd142018a453f3446e40
Collin Smith <cmsmith.tx@gmail.com>
2019-07-24 12:21:06 -0500

    Added delete button in object

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ $('#addTask').on('click', function(){
     toDoList.addTask();
 });
 
+$('#tasks').on('click', '#remove', function(){
+    toDoList.removeTask(this);
+});
+
 let toDoList = {
     tasks: [],
     addTask: function(){
@@ -10,10 +14,16 @@ let toDoList = {
             $('#tasks').append(`<li class="list-group-item">${userInput}<button id="remove" class="btn btn-danger">Remove</button></li>`);
             $('#remove').css("margin-left", '10.5rem');
             $('#task').css('border', '1px solid green');
+            $('#remove').css('font-size', '0.8rem');
             $('#task').val("");
+            this.tasks.push(userInput);
         } else {
             $('#task').css('border', '1px red solid');
             //window.alert("You can't add nothing to your to do list!");
         }
+    },
+    removeTask: function(element){
+        $(element).parent().remove();
     }
 }
+console.log(toDoList);
